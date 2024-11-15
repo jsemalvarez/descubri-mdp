c402968dac3c539946fda898082eb444a907b0ae
jsemalvarez <jsemalvarez@gmail.com>
2024-11-15 13:42:35 -0300

    feat: install react

---
+++ src/app/App.tsx
@@ -0,0 +1,10 @@
+import { Presentation } from '../features/presentations/Presentation'
+import './style/Global.css'
+
+export const App = () => {
+  return (
+    <>
+      <Presentation />
+    </>
+  )
+}
---
+++ src/features/presentations/Presentation.tsx
@@ -0,0 +1,12 @@
+import './Presentation.css'
+
+export const Presentation = () => {
+  return (
+    <div className="presentation">
+    <h1>
+      <span className="text-blue">Descubrí</span>
+      <span className="text-orange flicker">MdP</span>
+    </h1>
+  </div>
+  )
+}
