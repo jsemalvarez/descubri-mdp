6696282befc4173d8bb6db11d06fd7f5d73c99c6
jsemalvarez <jsemalvarez@gmail.com>
2024-11-28 18:57:40 -0300

    feat: dinamic authState in AppRoutes

--- src/app/routes/AppRoutes.tsx
+++ src/app/routes/AppRoutes.tsx
@@ -1,15 +1,23 @@
 import { Route, Routes } from "react-router-dom"
 import { PrivateRoutes } from "./PriovateRoutes";
 import { PublicRoutes } from "./PublicRoutes";
+import { useAuth } from "../../features/auth";
 
 export const AppRouter = () => {
 
-    const authStatus: string = 'authenticated'; // 'authenticated'; // 'not-authenticated';
+    // const authStatus: string = 'authenticated'; // 'authenticated'; // 'not-authenticated';
+    const { status: authStatus } = useAuth()
+
+    if(authStatus === 'CHECKING'){
+        return(<p>
+            Cargando....
+        </p>)
+    }
 
     return (
         <Routes>
             {
-                ( authStatus === 'not-authenticated')  
+                ( authStatus === 'NOT-AUTHENTICATED')  
                     ? <Route path="/*" element={ <PublicRoutes /> } />
                     : <Route path="/*" element={ <PrivateRoutes /> } />
             }
